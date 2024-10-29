6fda03b7ffe3b9ce578a992990d4bcf4ca55aceb
Hoàng Bách <hoangbach@MacBook-Pro-cua-Hoang.local>
2024-10-29 12:58:00 +0700

    add nhan.cpp file

--- nhan.cpp
+++ nhan.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main ()
 {
-    // phan khai bien cuc bo;
-    int a,b;
+    //  phan khai bien cuc bo;
+    int a, b;
     int c;
 
-    // phan khoi tao bien;
+    //phan khoi tao bien;
     a = 10;
     b = 2;
     c = a * b;
