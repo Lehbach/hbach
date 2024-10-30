f007cb3720fba41700bbac08ee85e8eeb1201880
Hoàng Bách <hoangbach@MacBook-Pro-cua-Hoang.local>
2024-10-31 00:12:46 +0700

    add tich.cpp file

---
+++ tich.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    int c;
+    cout << "gia tri cua a=";
+    cin >> a;
+    cout << "gia tri cua b=";
+    cin >> b;
+    c = a * b;
+    cout << "tich so = " << c << endl;
+}
