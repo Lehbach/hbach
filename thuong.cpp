30fb89df6d6b8c286341257be5c93cc8f2da1463
Hoàng Bách <hoangbach@MacBook-Pro-cua-Hoang.local>
2024-10-31 00:30:13 +0700

    add thuong.cpp file

---
+++ thuong.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    float c;
+    cout << "gia tri cua a=";
+    cin >> a;
+    cout << "gia tri cua b=";
+    cin >> b;
+    c = a / b;
+    cout << "thuong so = " << c << endl;
+}
