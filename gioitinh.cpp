ff7ef3eaba366d89ea608e5e612a9fa65335fe66
Lehbach <bachhoangle47@gmail.com>
2024-11-04 18:51:04 +0700

    Update gioitinh.cpp

--- gioitinh.cpp
+++ gioitinh.cpp
@@ -7,11 +7,11 @@ int main()
     cin >> a;
     if ((a = 1))
     cout << "ban la gioi tinh nam";
-    if ((a = 0))
+    else if ((a = 0))
     cout << "ban la gioi tinh nu";
-    if (a < 0)
+    else if (a < 0)
     cout << "moi ban nhap lai";
-    if (a > 1)
+    else if (a > 1)
     cout << "chua xac dinh";
 
 }
