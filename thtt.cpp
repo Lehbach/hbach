2e4c804f39737ec9e8fa6b26762b8baa8e207b96
Lehbach <bachhoangle47@gmail.com>
2024-11-05 17:32:07 +0700

    Update thtt.cpp

--- thtt.cpp
+++ thtt.cpp
@@ -1,49 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// ham tinh tong cua 2 so nguyen
-int tinhtong (int a, int b);
+int main ()
 {
-    return a + b;
-}
-
-// Ham tinh hieu cua 2 so nguyen
-int tinhhieu (int a, int b);
-{
-    return a - b;
-}
-// ham tinh tich cua 2 so nguyen
-int tinhtich (int a, int b);
-{
-    return a * b;
-}
-// Ham tinh thuong cua 2 so nguyen
-int tinhthuong (int a, int b);
-{
-    return a / b;
-}
-
-int main();
-{
-    int a, b;
-    cout << "gia tri cua a =";
+    float a, b, tong, hieu, tich, thuong;
+    cout << "nhap a =";
     cin >> a;
-    cout << "gia tri cua b =";
+    cout << "nhap b =";
     cin >> b;
 
-    int tong = tinhtong(a, b);
-    int hieu = tinhhieu(a, b);
-    int tich = tinhtich(a, b);
-    int thuong = tinhthuong(a, b);
+    tong = a + b;
+    hieu = a - b;
+    tich = a * b;
+    thuong = a / b;
 
-    cout << "Tổng của " << a << " và " << b << " là: " << tong << endl;
-    cout << "Hiệu của " << a << " và " << b << " là: " << hieu << endl;
-    cout << "Tích của " << a << " và " << b << " là: " << tich << endl;
-    cout << "Thương của " << a << " và " << b << " là:" << thuong << endl;
-
-    return 0;
+    cout << "tong so =" << tong << endl;
+    cout << "hieu so =" << hieu << endl;
+    cout << "tich so =" << tich << endl;
+    cout << "thuong so =" << thuong << endl;
 }
-
-
-
-
+    
